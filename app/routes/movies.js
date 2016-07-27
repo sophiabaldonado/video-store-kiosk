173bb406c80c6306e01ef2bad81e31a0bfdfa1f5
sophia <sophiabaldonado@gmail.com>
2016-07-27 16:52:52 -0700

    Movies display more info when clicked

--- app/routes/movies.js
+++ app/routes/movies.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    let movies = ['movie1', 'movie2']
-    // console.log($.getJSON("http://localhost:3000"))
     return $.getJSON("http://localhost:3000")
-    // return movies
   }
 });
